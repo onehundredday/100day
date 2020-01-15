e79ff104f7e9cac6a6639daec9e4f013bd6e161e
Islom Alimov <islomalimovm@gmail.com>
2020-01-15 20:34:28 +0100

    Swap Two Numbers

---
+++ main.py
@@ -0,0 +1,26 @@
+#O'zgaruvchilar
+x = 5
+y = 3
+
+#O'zgaruvchilarni Terminalga chiqarish
+print(f'Birinchi raqam x = {x}')
+print(f'Ikkinchi raqam y = {y}')
+
+vaqtinchalik = x
+x = y
+y = vaqtinchalik
+
+print(f'Birinchi raqam x = {x}')
+print(f'Ikkinchi raqam y = {y}')
+ 
+x = x + y  #x = 8 y = 5
+print(f'Birinchi raqam x = {x}')
+print(f'Ikkinchi raqam y = {y}')
+
+y = x-y # x = 8 y = 3
+print(f'Birinchi raqam x = {x}')
+print(f'Ikkinchi raqam y = {y}')
+
+x = x - y # = 5 y = 3
+print(f'Birinchi raqam x = {x}')
+print(f'Ikkinchi raqam y = {y}')
