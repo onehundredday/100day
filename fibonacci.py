d6d246f0e88e606354215671816dd2e2312e95b9
Islom Alimov <islomalimovm@gmail.com>
2020-01-19 23:37:00 +0100

    fibonacci numbers

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+# Input
+nterms = int(input("Terminalga son kiriting"))
+# Fibonacci sonlarinig 1 va 2 sonlari
+n1, n2 = 0, 1
+count = 0
+# kiritilgan soni tog'riligni tekshirish
+if nterms <= 0:
+   print("Iltimos musbat son kiriting")
+elif nterms == 1:
+   print("Fibonacci ketma ketligi:")
+   print(n1)
+else:
+   print("Fibonacci ketma ketligi:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
